6f6139d734c177c31a4bccb559e30d1f37552759
Alex <alexmore080@gmail.com>
2022-10-12 20:08:13 -0500

    index created

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/api',apiRoutes);
+router.use('/',homeRoutes);
 
 module.exports = router;
