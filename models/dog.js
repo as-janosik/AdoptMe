317dde98bbeb6322d644b15bf407a8caf39a1be3
Alex <alexmore080@gmail.com>
2022-10-12 19:17:13 -0500

    app name change/models structure started

---
+++ models/dog.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Dog extends Model {}
+
+Dog.init(
+    {
+        id: {
+            type: DataTypes.
+        },
+        dog_name: {
+
+        },
+        breed: {
+
+        },
+        dateAdded: {
+
+        },
+        available: {
+
+        },
+        addedBy: {
+
+        }
+    }
+)
