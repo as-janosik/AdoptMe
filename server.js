5acb17366ffcbb4be13681af06bf41fdb09323c6
Alex <alexmore080@gmail.com>
2022-10-13 21:23:31 -0500

    server js file fixed

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
 
+const sequelize = require('./config/connection');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname, 'views'));
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = routes;
