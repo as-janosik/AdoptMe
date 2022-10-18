5ccb2ed5164b480a5c532248636dfa1ccaa13400
Alex <alexmore080@gmail.com>
2022-10-17 20:50:26 -0500

    changed require to match 'htmlRoutes'

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const apiRoutes = require('./api') 
-const homeRoutes = require('./homeRoutes')
+const homeRoutes = require('./htmlRoutes')
 
 router.use('/api', apiRoutes);
 router.use('/',homeRoutes);
