b5b1cebb2950af3e4e2bd0e927a9c538663f4249
Alex <alexmore080@gmail.com>
2022-10-17 21:01:14 -0500

    sequelize/expresshandlebars/helpers added

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./controllers');
-const sequelize = require('sequelize');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+
 // view engine setup
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
